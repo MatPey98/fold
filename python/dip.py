572067e6d9f2ad49cecc9730f22a6308bc651329
MatPey98 <student@193.54.29.81>
2026-02-16 17:20:07 +0100

    change CTRL to LF separator

--- python/dip.py
+++ python/dip.py
@@ -1,1 +1,176 @@
-import mathimport osimport geopandasimport matplotlib.pyplot as pltimport numpy as npclass Dip:    """    Lis les points géographiques depuis un fichier SIG    Récupère les altitudes depuis le MNT    Ajuste un plan 3D sur les points (x, y, z)    Puis calcul le pendage de ce plan    """    def __init__(self, wdir):        """        Liste tous les fichiers d'un dossier et récupère les fichiers "shp"        :param wdir: répertoire de travail        """        self.wdir = wdir        items = os.listdir(self.wdir)        self.items = [item for item in items if item.endswith(".shp")]    def load_points(self, filename, mnt):        """        Charge les points pour placer le plan stratigraphique        :param filename: Nom du fichier contenant les points pour le pendage        :param mnt: Modèle d'élévation Pléiades        :return: Les points (x, y, z)        """        points = []        file = geopandas.read_file(self.wdir + filename)        for pt in file.geometry:            points.append([pt.x, pt.y])        for i in range(len(points)):            points[i].append(mnt.elevations([points[i]])[0])        self.points = points    def fit_plane(self):        """        Méthode des moindres carrées afin de fitter un plan au nuage de points        :return: les coefficients de l'équation du plan c = ax + by - z        """        points = np.array(self.points)        A = np.c_[points[:, 0], points[:, 1], - np.ones(points.shape[0])]        B = points[:, 2]        coefficients = np.linalg.lstsq(A, B, rcond=None)[0]        return coefficients    def incertitudes(self, a, b, c):        """        :param a:        :param b:        :param c:        :return:        """        points = np.array(self.points)        for i in range(len(points[0])):            incertitudes = sum(points[2][i] - (a * points[0][i] + b * points[1][i] - c))            return incertitudes    def calculate_dip(self, a, b, c):        """        Calcul le pendage par rapport à la norme "n" du plan et l'horizontale        :param a: coefficient a (est) de l'équation de plan        :param b: coefficient b (nord) de l'équation de plan        :param c: coefficient c de l'équation de plan        """        norm_n = np.sqrt(a ** 2 + b ** 2 + c ** 2)        cos_theta = c / norm_n        theta_rad = np.arccos(cos_theta)        theta_deg = np.degrees(theta_rad)        if theta_deg > 90: # Corriger les pendages au dessus de 90°, éviter les inversions de sens            theta_deg = 180 - theta_deg        print(f'dip : {theta_deg}')        self.dip = theta_deg    def calculate_azimuth(self, a, b):        """        Calcul de l'azimut du plan        :param a: coefficient a de l'équation de plan        :param b: coefficient b de l'équation de plan        """        azimut_rad = - math.atan2(a, b) # Valeur strike entre 0 et 90°        azimut_deg = math.degrees(azimut_rad)        if azimut_deg < 0: # Ajoute les valeurs de strike entre 270 et 360° -> complète les valeurs manquantes de strike            azimut_deg += 360        self.azimuth = azimut_deg    def projeter_pendage(self, profile):        """        :param profile: profile tracé sur un GIS        :return: Le pendage projeté sur ce profile        """        phi = abs(profile.azimuth - self.azimuth) % 360        if phi > 180:            phi = 360 - phi        pendage_reel_rad = np.radians(self.dip)        phi_rad = np.radians(phi)        pendage_projete_rad = np.arctan(np.tan(pendage_reel_rad) * np.cos(phi_rad))        pendage_projete_deg = np.degrees(pendage_projete_rad)        if pendage_projete_deg > 0:            pendage_projete_deg = -pendage_projete_deg        elif pendage_projete_deg < 0:            pendage_projete_deg = 180 - pendage_projete_deg        self.dip = pendage_projete_deg    def point_median(self):        """        :return:        """        points_array = np.array(self.points)        median_x = np.median(points_array[:, 0])        median_y = np.median(points_array[:, 1])        self.x, self.y = median_x, median_y    def find_intersection(self, profile):        """        :param profile:        :return:        """        if abs(self.dip) < 5:            m1 = 1 / np.tan(np.radians(profile.azimuth + 90))            m2 = 1 / np.tan(np.radians(profile.azimuth))            x = (m1 * self.x - self.y - m2 * profile.start_point[0] + profile.start_point[1]) / (m1 - m2)            y = m1 * (x - self.x) + self.y        else:            m1 = 1 / np.tan(np.radians(self.azimuth + 90))            m2 = 1 / np.tan(np.radians(profile.azimuth))            x = (m1 * self.x - self.y - m2 * profile.start_point[0] + profile.start_point[1]) / (m1 - m2)            y = m1 * (x - self.x) + self.y        self.intersect = [[x, y]]    def print(self, length_dip, x, y):        """        :param length_dip:        :param x:        :param y:        :return:        """        if self.azimuth >= 0:            dx = length_dip * np.cos(np.radians(self.dip))            dy = length_dip * np.sin(np.radians(self.dip))            x2 = x + dx            y2 = y + dy            plt.plot([x, x2], [y, y2], "k")    def print_all(self, topodata, profile, length_dip, x0):        """        Pour effectuer la projection des pendages        :param topodata:        :param profile:        :param length_dip:        :param x0:        :return:        """        for filename in self.items:            self.load_points(filename, topodata)            self.point_median()            a, b, c = self.fit_plane()            self.calculate_dip(a, b, -1)            self.calculate_azimuth(a, b)            self.projeter_pendage(profile)            self.find_intersection(profile)            #x = np.sqrt(self.intersect[0][0] ** 2 + self.intersect[0][1] ** 2) - x0            proj = profile.get_projection_all(self.intersect[0])            x = proj[0]            y = topodata.elevations(self.intersect)            if y[0] != "NaN":                self.print(length_dip, x, y)
+import math
+import os
+import geopandas
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class Dip:
+    """
+    Lis les points géographiques depuis un fichier SIG
+    Récupère les altitudes depuis le MNT
+    Ajuste un plan 3D sur les points (x, y, z)
+    Puis calcul le pendage de ce plan
+    """
+    def __init__(self, wdir):
+        """
+        Liste tous les fichiers d'un dossier et récupère les fichiers "shp"
+        :param wdir: répertoire de travail
+        """
+        self.wdir = wdir
+        items = os.listdir(self.wdir)
+        self.items = [item for item in items if item.endswith(".shp")]
+
+    def load_points(self, filename, mnt):
+        """
+        Charge les points pour placer le plan stratigraphique
+        :param filename: Nom du fichier contenant les points pour le pendage
+        :param mnt: Modèle d'élévation Pléiades
+        :return: Les points (x, y, z)
+        """
+        points = []
+        file = geopandas.read_file(self.wdir + filename)
+        for pt in file.geometry:
+            points.append([pt.x, pt.y])
+        for i in range(len(points)):
+            points[i].append(mnt.elevations([points[i]])[0])
+        self.points = points
+
+    def fit_plane(self):
+        """
+        Méthode des moindres carrées afin de fitter un plan au nuage de points
+        :return: les coefficients de l'équation du plan c = ax + by - z
+        """
+        points = np.array(self.points)
+        A = np.c_[points[:, 0], points[:, 1], - np.ones(points.shape[0])]
+        B = points[:, 2]
+        coefficients = np.linalg.lstsq(A, B, rcond=None)[0]
+        return coefficients
+
+    def incertitudes(self, a, b, c):
+        """
+
+        :param a:
+        :param b:
+        :param c:
+        :return:
+        """
+        points = np.array(self.points)
+
+        for i in range(len(points[0])):
+
+            incertitudes = sum(points[2][i] - (a * points[0][i] + b * points[1][i] - c))
+            return incertitudes
+
+    def calculate_dip(self, a, b, c):
+        """
+        Calcul le pendage par rapport à la norme "n" du plan et l'horizontale
+        :param a: coefficient a (est) de l'équation de plan
+        :param b: coefficient b (nord) de l'équation de plan
+        :param c: coefficient c de l'équation de plan
+        """
+        norm_n = np.sqrt(a ** 2 + b ** 2 + c ** 2)
+        cos_theta = c / norm_n
+        theta_rad = np.arccos(cos_theta)
+        theta_deg = np.degrees(theta_rad)
+        if theta_deg > 90: # Corriger les pendages au dessus de 90°, éviter les inversions de sens
+            theta_deg = 180 - theta_deg
+        print(f'dip : {theta_deg}')
+        self.dip = theta_deg
+
+    def calculate_azimuth(self, a, b):
+        """
+        Calcul de l'azimut du plan
+        :param a: coefficient a de l'équation de plan
+        :param b: coefficient b de l'équation de plan
+        """
+        azimut_rad = - math.atan2(a, b) # Valeur strike entre 0 et 90°
+        azimut_deg = math.degrees(azimut_rad)
+        if azimut_deg < 0: # Ajoute les valeurs de strike entre 270 et 360° -> complète les valeurs manquantes de strike
+            azimut_deg += 360
+        self.azimuth = azimut_deg
+
+    def projeter_pendage(self, profile):
+        """
+        :param profile: profile tracé sur un GIS
+        :return: Le pendage projeté sur ce profile
+        """
+        phi = abs(profile.azimuth - self.azimuth) % 360
+        if phi > 180:
+            phi = 360 - phi
+        pendage_reel_rad = np.radians(self.dip)
+        phi_rad = np.radians(phi)
+        pendage_projete_rad = np.arctan(np.tan(pendage_reel_rad) * np.cos(phi_rad))
+        pendage_projete_deg = np.degrees(pendage_projete_rad)
+        if pendage_projete_deg > 0:
+            pendage_projete_deg = -pendage_projete_deg
+        elif pendage_projete_deg < 0:
+            pendage_projete_deg = 180 - pendage_projete_deg
+        self.dip = pendage_projete_deg
+
+    def point_median(self):
+        """
+
+        :return:
+        """
+        points_array = np.array(self.points)
+        median_x = np.median(points_array[:, 0])
+        median_y = np.median(points_array[:, 1])
+        self.x, self.y = median_x, median_y
+
+    def find_intersection(self, profile):
+        """
+
+        :param profile:
+        :return:
+        """
+        if abs(self.dip) < 5:
+            m1 = 1 / np.tan(np.radians(profile.azimuth + 90))
+            m2 = 1 / np.tan(np.radians(profile.azimuth))
+            x = (m1 * self.x - self.y - m2 * profile.start_point[0] + profile.start_point[1]) / (m1 - m2)
+            y = m1 * (x - self.x) + self.y
+        else:
+            m1 = 1 / np.tan(np.radians(self.azimuth + 90))
+            m2 = 1 / np.tan(np.radians(profile.azimuth))
+            x = (m1 * self.x - self.y - m2 * profile.start_point[0] + profile.start_point[1]) / (m1 - m2)
+            y = m1 * (x - self.x) + self.y
+        self.intersect = [[x, y]]
+
+    def print(self, length_dip, x, y):
+        """
+
+        :param length_dip:
+        :param x:
+        :param y:
+        :return:
+        """
+        if self.azimuth >= 0:
+            dx = length_dip * np.cos(np.radians(self.dip))
+            dy = length_dip * np.sin(np.radians(self.dip))
+            x2 = x + dx
+            y2 = y + dy
+            plt.plot([x, x2], [y, y2], "k")
+
+    def print_all(self, topodata, profile, length_dip, x0):
+        """
+        Pour effectuer la projection des pendages
+        :param topodata:
+        :param profile:
+        :param length_dip:
+        :param x0:
+        :return:
+        """
+        for filename in self.items:
+            self.load_points(filename, topodata)
+            self.point_median()
+            a, b, c = self.fit_plane()
+            self.calculate_dip(a, b, -1)
+            self.calculate_azimuth(a, b)
+            self.projeter_pendage(profile)
+            self.find_intersection(profile)
+            #x = np.sqrt(self.intersect[0][0] ** 2 + self.intersect[0][1] ** 2) - x0
+            proj = profile.get_projection_all(self.intersect[0])
+            x = proj[0]
+            y = topodata.elevations(self.intersect)
+            if y[0] != "NaN":
+                self.print(length_dip, x, y)
