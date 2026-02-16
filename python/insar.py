eaabf0a5c2099d471f4006eb54e1bbbecba8d054
MatPey98 <student@193.54.29.81>
2026-02-16 17:28:05 +0100

    change CTRL to LF separator

--- python/insar.py
+++ python/insar.py
@@ -1,1 +1,34 @@
-import rasterioimport rasterio.windowsclass Insar:    def __init__(self, filename, wdir):        self.path = wdir + filename        self.raster = rasterio.open(self.path)    # def projection_insar(self):    #    # band = insar_data.raster.read(1)    # rows, cols = band.shape    #    # abscisses_insar = []    # velocities = []    #    # for row in range(rows):    #     for col in range(cols):    #    #         x, y = insar_data.raster.xy(row, col)    #         proj = profile.get_projection((x, y), width)    #    #         if proj is not None:    #    #             xpp, ypp = proj    #             value = band[row, col]    #    #             if not np.isnan(value):    #                 abscisses_insar.append(xpp)    #                 velocities.append(value)    #    # plt.scatter(abscisses_insar, velocities, s=2)
+import rasterio
+import rasterio.windows
+
+
+class Insar:
+
+    def __init__(self, filename, wdir):
+        self.path = wdir + filename
+        self.raster = rasterio.open(self.path)
+
+    # def projection_insar(self):
+    #
+    # band = insar_data.raster.read(1)
+    # rows, cols = band.shape
+    #
+    # abscisses_insar = []
+    # velocities = []
+    #
+    # for row in range(rows):
+    #     for col in range(cols):
+    #
+    #         x, y = insar_data.raster.xy(row, col)
+    #         proj = profile.get_projection((x, y), width)
+    #
+    #         if proj is not None:
+    #
+    #             xpp, ypp = proj
+    #             value = band[row, col]
+    #
+    #             if not np.isnan(value):
+    #                 abscisses_insar.append(xpp)
+    #                 velocities.append(value)
+    #
+    # plt.scatter(abscisses_insar, velocities, s=2)
