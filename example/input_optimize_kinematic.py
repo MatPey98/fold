4d7ce47300d2858c8a86cd15835438c4a75aae44
Simon Daout <simondaout@gmail.com>
2026-07-01 10:08:48 +0200

    ajoute fichier d'input de référence

--- example/input_optimize_kinematic.py
+++ example/input_optimize_kinematic.py
@@ -12,8 +12,8 @@ nbins = 50               # Interpolation step for the InSAR median
 # ======================================================================================================================
 # MCMC parameters
 # ======================================================================================================================
-niter = 4000  # Total number of iterations
-nburn = 3500   # Number of burn-in iterations
+niter = 2000  # Total number of iterations
+nburn = 1500   # Number of burn-in iterations
 n_samples = 500  # Number of posterior draws to display
 chains = 9     # Number of MCMC chains
 cores = 9      # Number of CPU cores used
@@ -45,15 +45,17 @@ Z_ref    = 3307.0  # Reference elevation of initial flat strata in kinematic.py 
 # Data paths
 # ======================================================================================================================
 wdir = './work/'
+output_dir = wdir + 'fold/output_2000iter/'
 
 ### Profile shapefile
 coupe            = "coupe12.shp"
 chemin_coupe     = wdir + "/carto/coupes/"
 
-### InSAR decomposed velocity fields
-insar_vertical   = "vertical_2003-2011_mm_crop03_UTM.tif"
-insar_horizontal = "shortening_2003-2011_mm_crop03_UTM.tif"
-chemin_insar     = wdir + "/data/insar/decomp2026/"
+### insar :
+insar_vertical = "vertical_2003-2019_mm_crop03_no2004_UTM.tif"
+insar_horizontal = "shortening_2003-2019_mm_crop03_no2004_UTM.tif"
+chemin_insar = wdir + "/data/insar/decomp2026/"
+
 
 ### Digital Elevation Model (DEM)
 mnt              = "cop_dem30_92_101_34_40_crop03_UTM.tif"
